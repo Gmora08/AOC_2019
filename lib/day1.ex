8ea795c9bd4b18a691aa967b31e676a27dd61c78
Gustavo Mora Gonzalez <gmora008@gmail.com>
2019-12-01 20:36:47 -0600

    solve day1 part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -18,10 +18,41 @@ defmodule Aoc2019.Day1 do
     end)
   end
 
+  @doc """
+  Calculates the cost of take a module
+
+  ## Examples
+
+    iex> Aoc2019.Day1.part2([1969])
+    966
+
+    iex> Aoc2019.Day1.part2([100756])
+    50346
+  """
+  def part2(mass_list) do
+    mass_list
+    |> Enum.map(&get_fuel_required_for_mass/1)
+    |> Enum.map(fn module_fuel ->
+      get_fuel_required_for_module(module_fuel, 0) + module_fuel
+    end)
+    |> Enum.sum()
+  end
+
   @spec get_fuel_required_for_mass(integer) :: integer
   defp get_fuel_required_for_mass(mass) do
     fuel = div(mass, 3)
 
     fuel - 2
+  end
+
+  @spec get_fuel_required_for_module(integer, integer) :: integer
+  defp get_fuel_required_for_module(mass, acc) do
+    fuel_required = get_fuel_required_for_mass(mass)
+
+    if fuel_required <= 0 do
+      acc
+    else
+      get_fuel_required_for_module(fuel_required, acc + fuel_required)
+    end
   end
 end
